a96e01667787d51904c92724f9990f7dc8b7201c
FGARMY <flaminggarena@gmail.com>
2026-09-02 15:43:55 +0530

    Fix TypeScript error in layout.tsx TooltipProvider prop

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
           enableSystem
           disableTransitionOnChange
         >
-          <TooltipProvider delayDuration={0}>
+          <TooltipProvider delay={0}>
             <div className="flex h-full">
               <Sidebar />
               <div className="flex-1 flex flex-col min-w-0">
